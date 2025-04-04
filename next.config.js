f5e26d77b1accf6fc6ef9a8764fc463ad0ed8f42
Brennan Mattingly <mattinglybrennan@gmail.com>
2025-04-04 15:22:13 -0500

    Fix path alias in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['placeholder.com'],
   },
-}
-module.exports = nextConfig
+  webpack: (config) => {
+    // Add the alias for '@'
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+};
+
+module.exports = nextConfig;
